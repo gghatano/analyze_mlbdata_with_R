db0860dbd3b4814251b0e30ed51b7631b0a3816f
gghatano <adad.0405@gmail.com>
2014-04-10 18:14:18 +0900

    use pitchRx

--- pitchRx/darvish_data.R
+++ pitchRx/darvish_data.R
@@ -1,6 +1,24 @@
 library(pitchRx)
 
-dat = fread("darvis2013.csv") 
+dat = fread("darvish_data.csv") 
 
+# 公式記録のあるデータを使う
+dat = dat %>% filter(sv_id != "NA")
 
+# make the pitching ball trajectry by using rgl
+dat %>% filter(sv_id != "NA") %>%
+  filter(pitch_type %in% c("SL", "FF")) %>% 
+  head(20) %>% interactiveFX
+
+# make animation of the pitching
+animation::saveHTML({
+dat %>% filter(sv_id != "NA") %>%
+  filter(pitch_type %in% c("SL", "FF")) %>% head(30) %>% 
+  animateFX(interval = 0.01, point.alpha = 0.9, layer=facet_grid(pitcher_name~stand))
+}, htmlfile = "pitching_animation.html", outdir = getwd())
+
+# density plot
+strikeFX(dat, geom="tile", 
+         density1=list(des="Called Strike"), 
+         density2=list(des="Ball"), layer=facet_grid(.~stand))
 
