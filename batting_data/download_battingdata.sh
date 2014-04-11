c3e78e1c3814b7a7c42c4cd9c41ed663cd1a3b1d
gghatano <adad.0405@gmail.com>
2014-04-11 10:38:53 +0900

    modify the download_battingdata.sh

--- batting_data/download_battingdata.sh
+++ batting_data/download_battingdata.sh
@@ -1,22 +1,28 @@
-# 
+### download_battingdata.sh
 
+# make urls
 url1="http://www.retrosheet.org/events/"
 year=$1
 url2="eve.zip"
+
 url_ful=$url1$year$url2
 
+# download the raw data from retrosheet.org
 wget $url_ful
 
+# unzip
 unzip $year"eve.zip"
 
+# make the datafile.csv
 csv_name="all"$year".csv"
 touch $csv_name
 
+# parse and transform the raw data by using cwevent
 cw_option=" -y "$year" -f 0-96 "$year"*.EV*"  
 echo $cw_option
-
 cwevent $cw_option > $csv_name
 
+# removing the needless data
 rm *$year".ROS"
 rm $year*".EV"*
 rm "TEAM"*
