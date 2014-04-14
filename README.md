385d229593f615cdca2f7afda8af6d254ebbf258
gghatano <adad.0405@gmail.com>
2014-04-14 09:31:49 +0900

    use rgl image

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Rでメジャーリーグのデータ解析をします.
 
 http://gg-hogehoge.hatenablog.com
 
+
+pitchRx: 投球データ取得, 解析
+batting_data: 打席結果データ解析
+shiny: shinyでwebアプリ作成
+rCharts: かっこいいグラフ
+
+
--- pitchRx/darvish_data/darvish_data.R
+++ pitchRx/darvish_data/darvish_data.R
@@ -1,4 +1,5 @@
 library(pitchRx)
+library(rgl)
 
 dat = fread("darvish_data.csv") 
 
@@ -8,14 +9,15 @@ dat = dat %>% filter(sv_id != "NA")
 # make the pitching ball trajectry by using rgl
 dat %>% filter(sv_id != "NA") %>%
   filter(pitch_type %in% c("SL", "FF")) %>% 
-  head(20) %>% interactiveFX
+  head(100) %>% interactiveFX
+writeWebGL(width=500, height=550)
 
 # make animation of the pitching
-animation::saveHTML({
-dat %>% filter(sv_id != "NA") %>%
-  filter(pitch_type %in% c("SL", "FF")) %>% head(30) %>% 
-  animateFX(interval = 0.01, point.alpha = 0.9, layer=facet_grid(pitcher_name~pitch_type))
-}, htmlfile = "pitching_animation.html", outdir = getwd())
+# animation::saveHTML({
+# dat %>% filter(sv_id != "NA") %>%
+#   filter(pitch_type %in% c("SL", "FF")) %>% head(30) %>% 
+#   animateFX(interval = 0.01, point.alpha = 0.9, layer=facet_grid(pitcher_name~pitch_type))
+# }, htmlfile = "pitching_animation.html", outdir = getwd())
 
 # density plot
 
