95a84174f73cf6f0b1d1c8ffd6a3b956c7c7f700
gghatano <adad.0405@gmail.com>
2014-04-25 13:03:37 +0900

    gamedata_analysis

---
+++ batting_data/game_analysis/gamedata_analysis.R
@@ -0,0 +1,21 @@
+library(data.table)
+library(magrittr)
+library(dplyr)
+
+res = fread("gamedata.csv")
+
+res_games = res %>% 
+  group_by(home, away) %>% 
+  dplyr::summarise(game = sum(game))
+res_games %>% arrange(desc(game)) 
+
+
+res_games_hl = 
+  res_games %>% 
+  mutate(h_l= ifelse(home > away, 
+                     paste(home, "-", away, sep=""),
+                     paste(away, "-", home, sep=""))) %>% 
+  group_by(h_l, add=FALSE) %>% 
+  dplyr::summarise(game = sum(game)) 
+res_games_hl %>% arrange(desc(game))
+res_games_hl$game %>% sum
