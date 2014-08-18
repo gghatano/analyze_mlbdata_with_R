7624c271ab203536065b587eb767aa14e647a690
gghatano <adad.0405@gmail.com>
2014-08-18 19:17:04 +0900

    BUG FIX

--- batting_data/game_analysis/win_prob/calc_win_prob.R
+++ batting_data/game_analysis/win_prob/calc_win_prob.R
@@ -1,13 +1,13 @@
 ## ------------------------------------------------------------------------
 ## 試合状況を入力すると, ホームの勝率を返す関数win_prob
+## ...を作ろうかと思いましたが, 端末から呼ぶとバグるのでやめます. 
+## シェルスクリプトから呼びやすくなるように, 
+## ./win_prob.csvを変形して./yahoo_sokuhou/win_prob.csvに入れます. 
+
 library(data.table)
 library(dplyr)
 library(stringr)
-# このファイルがあるディレクトリ
-dir <- (function() {
-  path <- (function() attr(body(sys.function(-1)), "srcfile"))()$filename
-  dirname(path)
-})()
+dir = "/Users/taku/works/analyze_mlbdata_with_R/batting_data/game_analysis/win_prob/"
 dat = fread(paste(dir, "/win_prob.csv", sep=""))
 dat %>% select()
 base_mutate = function(base = 10){
@@ -19,45 +19,12 @@ base_mutate = function(base = 10){
   base_situation
 }
 base_mutate(0)
+
+## データの変形
 dat %>% 
   mutate(RUNNERS = base_mutate(RUNNERS)) %>% 
   mutate(HOME_WIN_PROB = round(HOME_WINS/GAMES, 3)*100) %>% 
-  mutate(AWAY_WIN_PROB = (100 - HOME_WIN_PROB)) %>% 
+  mutate(AWAY_WIN_PROB = round((100 - HOME_WIN_PROB, 1))) %>% 
   write.csv("./yahoo_sokuhou/win_prob.csv", quote=FALSE, row.names=FALSE)
   
-stop()
 
-win_prob = function(INN_CT_ARG=1, BAT_HOME_ID_ARG=0, OUTS_CT_ARG=0, RUNNERS_ARG=0, HOME_AWAY_ARG=0){
-  dat_win_prob =
-    dat %>% 
-    filter(INN_CT == INN_CT_ARG, 
-           BAT_HOME_ID == BAT_HOME_ID_ARG, 
-           OUTS_CT == OUTS_CT_ARG, 
-           RUNNERS == RUNNERS_ARG, 
-           HOME_AWAY == HOME_AWAY_ARG)  
-  
-    if(dim(dat_win_prob)[2]==0){
-      return("データがありません")
-    } else{
-      dat_win_prob %>% 
-        group_by(add=FALSE) %>%
-        dplyr::summarise(HOME_WINS/GAMES) %>% 
-        unlist %>% 
-        return
-    }
-}
-
-situation = fread(paste(dir,"/yahoo_sokuhou/out3.txt", sep="")) %>% unlist
-
-## ランナー表記の調整 どうしてこうなったのか
-first = (situation[4] %>% as.character %>% str_detect("1")) * 1 
-second = (situation[4] %>% as.character %>% str_detect("2")) * 10 
-third = (situation[4] %>% as.character %>% str_detect("3")) * 100 
-situation[4] = first + second + third
-situation[4] = ifelse(is.na(situation[4]), 0, situation[4])
-
-## イニング, 裏表, アウト, ランナー状態, 点差を入力
-prob = win_prob(INN_CT = situation[1], BAT_HOME_ID_ARG = situation[2], OUTS_CT_ARG = situation[3], RUNNERS_ARG = situation[4], HOME_AWAY_ARG = situation[7] - situation[6])
-prob = round(prob, 3)
-win_prob_set = c(1 - prob, prob)
-win_prob_set %>% write.table(paste(dir, "/yahoo_sokuhou/winProb.txt", sep=""), row.names=FALSE, col.names=FALSE, quote=FALSE)
--- batting_data/game_analysis/win_prob/yahoo_sokuhou/baseball_jikkyou_tweet.bash
+++ batting_data/game_analysis/win_prob/yahoo_sokuhou/baseball_jikkyou_tweet.bash
@@ -7,6 +7,7 @@ echo $(date +%Y%m%d%H%M%S)
 ## 実況
 $dir/baseball_jikkyou.bash
 echo "jikkyou OK" >> $HOME/Dropbox/cron.log.txt
+echo "jikkyou OK" 
 
 ## 勝率計算
 ### situationに合致する勝率をwin_prob.csvから探す
@@ -14,24 +15,20 @@ situation=$(cat $dir/out.txt |
 xargs echo -n | 
 sed -e 's/無し/0/' -e 's/表/ 0/' -e 's/裏/ 1/' -e 's/ /;/g' -e 's/-/;/' -e 's/[^0-9;]//g')
 
-echo $situation 
-
-### 点差の修正
+### 点差の修正 10点差以上は10点差です. 
 scoreDiff=$(echo $situation | awk -F ";" '{print $7-$6}')
-echo $scoreDiff
 [ $scoreDiff -lt -10  ] && scoreDiff=-10
 [ $scoreDiff -gt 10  ] && scoreDiff=10
-echo $scoreDiff
 
+## 試合状況から勝率を探します
 situation=$(echo $situation | awk -F";" '{print $1, $2, $3, $4}'| sed 's/ /,/g')
-echo $situation
-echo $situation,$scoreDiff
 winProb=$(cat $dir/win_prob.csv | grep -E "^$situation,$scoreDiff,")
-echo $winProb
 winProb1=$(echo $winProb|awk -F "," '{print $10}')
 winProb2=$(echo $winProb|awk -F "," '{print $9}')
 [ "$winProb1" = "" ] && exit 1
 [ "$winProb2" = "" ] && exit 1
+
+echo "win_prob OK"
 
 ## チーム名の取得
 team1=$(cat $dir/out.txt | tail -n 1 | awk '{print $1}') 
@@ -43,11 +40,13 @@ echo "" >> $dir/tweet.txt
 echo ${team1}の勝率: $winProb1"%" >> $dir/tweet.txt
 echo ${team2}の勝率: $winProb2"%" >> $dir/tweet.txt
 
-## ツイートスクリプト生成
+
+## ツイートしてくれるRスクリプトを生成
 $(which filehame) -lLABEL $HOME/kousien_jikkyou_template.R $dir/tweet.txt > ~/kousien_jikkyou.R
 
 echo "filehame OK" >> $HOME/Dropbox/cron.log.txt
 
-## ツイート
+## ツイートします
 $(which R) -q --slave --vanilla -f ~/kousien_jikkyou.R
+
 echo "tweet OK" >> $HOME/Dropbox/cron.log.txt
