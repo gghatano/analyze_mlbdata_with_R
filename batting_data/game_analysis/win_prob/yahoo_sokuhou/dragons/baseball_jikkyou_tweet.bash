e36f39c58d1ef1c65cec9f33e3856b5d4c9674b8
gghatano <adad.0405@gmail.com>
2014-08-29 14:22:13 +0900

    dragonsbot作った

--- batting_data/game_analysis/win_prob/yahoo_sokuhou/dragons/baseball_jikkyou_tweet.bash
+++ batting_data/game_analysis/win_prob/yahoo_sokuhou/dragons/baseball_jikkyou_tweet.bash
@@ -6,7 +6,7 @@ set -e
 echo $(date +%Y%m%d%H%M%S)
 ## 実況
 $dir/baseball_jikkyou.bash
-echo "jikkyou OK" >> $HOME//cron.log.txt
+echo "jikkyou OK" >> $HOME/Dropbox/cron.log.txt
 echo "jikkyou OK" 
 
 ## 勝率計算
@@ -19,13 +19,14 @@ sed -e 's/無し/0/' -e 's/表/ 0/' -e 's/裏/ 1/' -e 's/ /;/g' -e 's/-/;/' -e '
 scoreDiff=$(echo $situation | awk -F ";" '{print $7-$6}')
 [ $scoreDiff -lt -10  ] && scoreDiff=-10
 [ $scoreDiff -gt 10  ] && scoreDiff=10
+echo modify_ok
 
 ## 試合状況から勝率を探します
 situation=$(echo $situation | awk -F";" '{print $1, $2, $3, $4}'| sed 's/ /,/g')
+echo $situation
 winProb=$(cat $dir/win_prob.csv | grep -E "^$situation,$scoreDiff,")
 winProb1=$(echo $winProb|awk -F "," '{print $10}')
 winProb2=$(echo $winProb|awk -F "," '{print $9}')
-echo "$situation"
 [ "$winProb1" = "" ] && exit 1
 [ "$winProb2" = "" ] && exit 1
 
@@ -41,17 +42,17 @@ echo "" >> $dir/tweet.txt
 echo ${team1}の勝率: $winProb1"%" >> $dir/tweet.txt
 echo ${team2}の勝率: $winProb2"%" >> $dir/tweet.txt
 
-echo "text_ok"
+echo make_text_ok
 ## ツイートしてくれるRスクリプトを生成
-$(which filehame) -lLABEL $HOME/kousien_jikkyou_template.R $dir/tweet.txt > ~/kousien_jikkyou.R
+echo $dir
+filehame -lLABEL $HOME/dragons_sokuhou_template.R $dir/tweet.txt > ~/dragons_sokuhou.R
+echo "filehame OK" >> $HOME/Dropbox/cron.log.txt
 
-echo "filehame OK"
 set +e
-label=$(cat ~/kousien_jikkyou.R | grep  "LABEL")
-set -e
+label=$(cat ~/dragons_sokuhou.R | grep  "LABEL")
 [ "$label" = "" ] || exit 1
 
 ## ツイートします
-$(which R) -q --slave --vanilla -f ~/kousien_jikkyou.R
+$(which R) -q --slave --vanilla -f ~/dragons_sokuhou.R
+echo "tweet OK" >> $HOME/Dropbox/cron.log.txt
 
-echo "tweet OK" >> $HOME//cron.log.txt
